8ec2a7b3cf60f03c9eb3cb8a3969f073a60ab113
Ema <emanuelgarciarios@gmail.com>
2024-09-17 13:06:32 -0500

    Add authenticate, get_access_token method

--- model/cine_tracker.py
+++ model/cine_tracker.py
@@ -15,3 +15,30 @@ class TraktAPI:
         self.TOKEN_URL = 'https://api.trakt.tv/oauth/token'
         self.API_URL = 'https://api.trakt.tv'
         self.access_token = None
+
+    def authenticate(self):
+        """Redirige al usuario a la URL de autorización y solicita el código de autorización."""
+        authorization_url = f'{self.AUTH_URL}?response_type=code&client_id={self.CLIENT_ID}&redirect_uri={self.REDIRECT_URI}'
+        webbrowser.open(authorization_url)
+        auth_code = input('Introduce el código de autorización que obtuviste de Trakt: ')
+        return self.get_access_token(auth_code)
+
+    def get_access_token(self, auth_code):
+        """Intercambia el código de autorización por un token de acceso."""
+        data = {
+            'code': auth_code,
+            'client_id': self.CLIENT_ID,
+            'client_secret': self.CLIENT_SECRET,
+            'redirect_uri': self.REDIRECT_URI,
+            'grant_type': 'authorization_code',
+        }
+        response = requests.post(self.TOKEN_URL, json=data)
+        if response.status_code == 200:
+            token_data = response.json()
+            self.access_token = token_data['access_token']
+            print(f"Access Token: {self.access_token}")
+            return True
+        else:
+            print(f"Error al obtener el token: {response.status_code}")
+            print(f"Respuesta: {response.text}")
+            return False
